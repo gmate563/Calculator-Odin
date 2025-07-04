b0c3c6495a6c8e447afcf92bfd94454e2a601fb7
gmate563 <197899324+gmate563@users.noreply.github.com>
2025-07-04 11:48:57 +0200

    Add operate func

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function divide (dividend, divisor){
     return dividend / divisor;
 }
 
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
+function operate (op, n1, n2){
+    if (op == "+"){
+        return add(n1, n2);
+    }
+    else if (op == "-"){
+        return subtract(n1, n2);
+    }
+    else if (op == "*"){
+        return multiply(n1, n2);
+    }
+    else{
+        return divide(n1, n2);
+    }
+}
+
