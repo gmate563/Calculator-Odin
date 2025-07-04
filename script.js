cc87662ed1b67d716bb0d36d22f64b5240d41691
gmate563 <197899324+gmate563@users.noreply.github.com>
2025-07-04 11:33:08 +0200

    Add calc functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add (num1, num2){
+    return num1 + num2;
+}
+
+function subtract (minuend, subtrahend){
+    return minuend - subtrahend;
+}
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+function divide (dividend, divisor){
+    return dividend / divisor;
+}
+
